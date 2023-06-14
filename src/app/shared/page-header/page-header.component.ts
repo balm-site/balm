90735c722bfa5b6e83997247560954167ad56205
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2023-06-14 22:46:43 +0100

    feedback

--- src/app/shared/page-header/page-header.component.ts
+++ src/app/shared/page-header/page-header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 import { ScrollToElementDirective } from '../scroll-to-element/scroll-to-element.directive';
 import { PageHeaderService } from './page-header.service';
 
@@ -13,10 +14,14 @@ import { PageHeaderService } from './page-header.service';
 })
 export class PageHeaderComponent implements OnInit {
   private readonly _pageHeader = inject(PageHeaderService);
+  private readonly _router = inject(Router);
   expanded = false;
   condensed = false;
 
   ngOnInit(): void {
     this._pageHeader.collapse$.subscribe(() => (this.expanded = false));
+    this._router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => (this.expanded = false));
   }
 }
